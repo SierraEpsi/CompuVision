8426c7f80eaa08f981fb78473e4fbdab06825deb
sebbe <cantiseba@gmail.com>
2017-05-10 13:15:26 +0200

    image loader added

--- image_loader.py
+++ image_loader.py
@@ -1,9 +1,11 @@
 
+import cv
 import cv2
 import math
 
 # this part of the code is only executed if the file is run stand-alone
 if __name__ == '__main__':
+    ddepth = cv2.CV_16S;
     # read an image
     img = cv2.imread('_Data/Radiographs/01.tif')
 
@@ -25,7 +27,12 @@ if __name__ == '__main__':
     cv2.waitKey(0)
 
     # look for edges
-    img = cv2.Canny(img, 20, 10)
+    img = cv2.Canny(img, 20, 15)
+    # img_x = img
+    # img_y = img
+    # cv.Sobel(img, img_x, ddepth, 1, 0);
+    # cv.Sobel(img, img_y, ddepth, 0, 1);
+    # cv.addWeighted(img_x, 0.5, img_y, 0.5, 0, img);
 
     # show the smoothed image, and wait for a key to be pressed
     cv2.imshow('img', img)
