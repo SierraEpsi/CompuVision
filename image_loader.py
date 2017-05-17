8e55f87bd23f6fd033fefb83a255b0fd46654d48
Mathieuv <mathieu_vermeire@hotmail.com>
2017-05-17 18:48:34 +0200

    filters

--- image_loader.py
+++ image_loader.py
@@ -1,27 +1,66 @@
 
+import numpy as np
 import cv
 import cv2
 import math
 
 # this part of the code is only executed if the file is run stand-alone
 if __name__ == '__main__':
-    ddepth = cv2.CV_16S;
+    ddepth = cv2.CV_64F;
     cv2.namedWindow("img1", cv2.WINDOW_NORMAL)
     cv2.namedWindow("img2", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("img3", cv2.WINDOW_NORMAL)
+    cv2.namedWindow("img4", cv2.WINDOW_NORMAL)
+
+
     # read an image
     img = cv2.imread('_Data/Radiographs/02.tif')
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    img = cv2.medianBlur(img, 11)
-    img = cv2.bilateralFilter(img, 9, 200, 150)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cv2.imshow('img1', img)
 
-    img = cv2. equalizeHist(img)
-    cv2.imshow('img2', img)
+    img_Blurred = cv2.medianBlur(img, 11)
+    #cv2.GaussianBlur(img,(5,5),0,img,0,cv2.BORDER_DEFAULT)
+
+    cv2.imshow('img2', img_Blurred)
+
+    #img = cv2. equalizeHist(img)
+    #cv2.imshow('img3', img)
+
+    img2 = cv2.Laplacian(img_Blurred,ddepth,ksize=7,scale=-1,delta=10)
+    cv2.imshow('img4', img2)
+
+    # Convert to 8 bit, doesnt work..
+    # img_8U = np.array(img2, dtype=np.uint8)
+    # cv2.imshow('img5', img_8U)
+
+    # 1. Do canny (determine the right parameters) on the gray scale image
+    t1 = 100
+    t2 = 50
+    edges = cv2.Canny(img_8U, t1, t2)
+
+    # Show the results of canny
+    canny_result = np.copy(img)
+    canny_result[edges.astype(np.bool)] = 0
+    cv2.imshow('img1', canny_result)
     cv2.waitKey(0)
 
-    img = cv2.adaptiveThreshold(img, 256, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, 0)
-    cv2.imshow('img1', img)
-    cv2.waitKey(0)
+
+
+    #img = cv2.medianBlur(img, 11)
+    #img = cv2.bilateralFilter(img, 15, 200, 150)
+    #cv2.imshow('img1', img)
+
+    #img = cv2. equalizeHist(img)
+    #cv2.imshow('img2', img)
+    #cv2.waitKey(0)
+
+    #img = cv2.adaptiveThreshold(img, 256, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, 0)
+    #cv2.imshow('img1', img)
+    #cv2.waitKey(0)
+
+    #  img = cv2.Laplacian(img,cv2.CV_64F,3)
+    #  cv2.imshow('img1', img)
+    #  cv2.waitKey(0)
 
     # # show the image, and wait for a key to be pressed
     # cv2.namedWindow("img", cv2.WINDOW_NORMAL)
