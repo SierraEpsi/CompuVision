368e93f75fbe97b9744bbd07733abeed2cf54bd9
Mathieuv <mathieu_vermeire@hotmail.com>
2017-06-07 15:57:56 +0200

    small changes

--- ASM.py
+++ ASM.py
@@ -18,75 +18,52 @@ p3 = fig.add_subplot(133)
 def load_landmarks(path):
     f = open(path, 'r')
     landmarks = np.loadtxt(f)
-    landmarks = np.reshape(landmarks, (landmarks.size / 2, 2))  # shape (40,2)
+    landmarks = np.reshape(landmarks, (landmarks.size / 2,2))  # shape (40,2)
     return landmarks
 
 def center_landmarks(landmarks):
-    x = landmarks[:, 0]
-    y = landmarks[:, 1]
+    x = landmarks[:,0]
+    y = landmarks[:,1]
     x = np.subtract(x,np.mean(x))
     y = np.subtract(y,np.mean(y))
     p1.plot(x, y, '*')
     p1.plot(x, y)
-    return x, y
+    array = np.zeros(landmarks.shape)
+    array[:,0] = x
+    array[:,1] = y
+    return array
 
 def rotate_shape(landmarks):
-    cov = np.dot(landmarks,np.transpose(landmarks))
-    n = landmarks[0].size
+    cov = np.dot(landmarks.T,landmarks)
+    n = landmarks.shape[0]
     cov = np.divide(cov,n-1)
     eigW, eigV = la.eig(cov)
     eigV = eigV/la.norm(eigV)
-    landmarks = np.dot(eigV,landmarks)
-    x = landmarks[0,:]
-    y = landmarks[1,:]
+    rotated = np.dot(landmarks,eigV)
+    x = rotated[:,0]
+    y = rotated[:,1]
     p2.plot(x, y, '*')
     p2.plot(x, y)
-    return landmarks
+    return rotated
 
 def scale_estimate(landmarks):
-    x = landmarks[0]
-    y = landmarks[1]
+    x = landmarks[:,0]
+    y = landmarks[:,1]
     scale_factor = np.sqrt(np.sum(np.power(x,2)) + np.sum(np.power(y,2)))
     x = np.divide(x,scale_factor)
     y = np.divide(y,scale_factor)
     p3.plot(x, y, '*')
     p3.plot(x, y)
-    return x, y
+    array = np.zeros(landmarks.shape)
+    array[:,0] = x
+    array[:,1] = y
+    return array
 
 def trans_rot(landmarks):
     landmarks = center_landmarks(landmarks)
-
     landmarks = rotate_shape(landmarks)
-
     landmarks = scale_estimate(landmarks)
-
     return landmarks
-
-def scale_estimate_Math(listOfLandmarks):
-    listOfAreas = []
-
-    for i in xrange(len(listOfLandmarks)):
-        landmarks = listOfLandmarks[i]
-        landmarks = np.reshape(landmarks,(1,landmarks.size))
-        listOfAreas += cv2.contourArea(landmarks)
-
-    meanArea = np.mean(listOfAreas)
-
-    for i in xrange(len(listOfLandmarks)):
-        landmarks = listOfLandmarks[i]
-        scale_factor = listOfAreas/meanArea
-        x = landmarks[0]
-        y = landmarks[1]
-        x = x*scale_factor
-        y = y*scale_factor
-        listOfLandmarks[i][0] = x
-        listOfLandmarks[i][1] = y
-
-        p3.plot(x, y, '*')
-        p3.plot(x, y)
-
-
-    return listOfLandmarks
 
 def pca(teeth, nbImgs):
     mu = np.mean(teeth, axis=0)
@@ -114,23 +91,17 @@ def reconstruct(Y, eigV, mu):
 
 def computeModel(folderWithLandmarks,toothNbr,nbImgs,nbDims):
 
-
-
     X = np.zeros((nbImgs, 2 * nbDims))
     folder = folderWithLandmarks
-    #listOfLandmarks = []
 
     for i in range(1, nbImgs + 1):
         path = folder + 'landmarks' + str(i) + '-' + str(toothNbr) + '.txt'
         landmarks = load_landmarks(path)
         landmarks = trans_rot(landmarks)
-        x = landmarks[0]
-        y = landmarks[1]
+        x = landmarks[:,0]
+        y = landmarks[:,1]
         X[i - 1, 0:nbDims] = x
         X[i - 1, nbDims:] = y
-        #listOfLandmarks +=  [landmarks]
-
-   # listOfLandsmarks  = scale_estimate_Math(listOfLandmarks)
 
     plt.show()
 
@@ -155,11 +126,13 @@ if __name__ == '__main__':
 
     eV,eW,mu = computeModel(folder,tooth,nbImgs,nbDims)
 
-    # Wrong example??
-    example = np.reshape(load_landmarks('_Data/landmarks/original/landmarks1-1.txt'),(1,nbDims*2))[0]
+    # need to trans rot first!
+    example = load_landmarks('_Data/landmarks/mirrored/landmarks15-1.txt')
+    example = trans_rot(example)
+    example = np.reshape(example.T,(example.size))
     example = project(example,eV,mu)
     example = reconstruct(example,eV,mu)
-    plt.plot(example[0:nbDims],example[nbDims:])
+    plt.plot(example[:nbDims],example[nbDims:])
     plt.show()
 
     print(example)
