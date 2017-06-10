aaf29bdf6ecd11f0239b2a39c9416c19142f7a54
Mathieuv <mathieu_vermeire@hotmail.com>
2017-06-10 18:24:35 +0200

    small changes

--- ImgPP.py
+++ ImgPP.py
@@ -1,90 +1,56 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
 
 def PPimg(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.medianBlur(img,5)
+    img = cv2.medianBlur(img,7)
     img = cv2.bilateralFilter(img,9,500,500)
-
-    clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(16, 16))
-    img = clahe.apply(img)
 
     imgX1 = cv2.Scharr(img,-1,1,0)
     imgX2 = cv2.Scharr(cv2.flip(img,1),-1,1,0)
     imgX2 = cv2.flip(imgX2,1)
-    imgX = cv2.addWeighted(imgX1, 0.5, imgX2, 0.5, 0)
+    imgX = (imgX1 + imgX2)/2
 
     imgY1 = cv2.Scharr(img,-1,0,1)
     imgY2 = cv2.Scharr(cv2.flip(img,0),-1,0,1)
     imgY2 = cv2.flip(imgY2,0)
-    imgY = cv2.addWeighted(imgY1, 0.5, imgY2, 0.5, 0)
+    imgY = (imgY1 + imgY2)/2
 
-    img = cv2.addWeighted(imgX, 0.5, imgY, 0.5, 0)
-    img = cv2.equalizeHist(img)
-    # img = cv2.threshold(img, 30, 256, cv2.THRESH_BINARY)[1]
+    img = (imgX + imgY)/2
+    clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(25, 25))
+    img = clahe.apply(img)
+    img = cv2.medianBlur(img, 11)
+    img = cv2.threshold(img, 30, 256, cv2.THRESH_BINARY)[1]
+    cv2.namedWindow('img', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('img', 1500, 1000)
+
+    img = cv2.erode(img, (5, 5), iterations=1)
+
     t1 = 100
-    t2 = 50
+    t2 = 0
     img = cv2.Canny(img, t1, t2, apertureSize=3)
+
 
     return img
 
-# Used to track mouse
 def getMouseCoord(event, x, y, flags, params):
     global mouseX, mouseY, refPt
     if event == cv2.EVENT_LBUTTONDBLCLK:
         mouseX, mouseY = x, y
     elif event == cv2.EVENT_LBUTTONDOWN:
-        if len(refPt) == 2:
-            refPt[0] = refPt[1]
-            refPt[1] = (x,y)
-        else:
-            refPt.append((x,y))
+        refPt.append((x, y))
 
 def selectSqr(img):
-
-    global mouseX, mouseY, refPt
-    mouseX = -1
-    mouseY = -1
-    refPt = []
-
     cv2.namedWindow('img', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('img', 1500, 1000)
-    cv2.setMouseCallback('img', getMouseCoord)
-    cv2.imshow('img', img)
-
-    doClose = False
+    cv2.imshow('img',img)
     while True:
         k = cv2.waitKey(0) & 0xFF
         # if the window is closed or 'esc' is pressed stop
         if k == 255 or k == 27:
-            doClose = True
             break
-        # check if spacebar is pressed
-        else:
-            cv2.imshow('img', img)
-            # press space to continue
-            if k == 32:
-                if len(refPt) == 2 or (mouseX != -1 and mouseY != -1):
-                    break
-    if doClose:
-        return
-
-    if len(refPt) == 2:
-        x1 = np.min((refPt[0][1], refPt[1][1]))
-        x2 = np.max((refPt[0][1], refPt[1][1]))
-        y1 = np.min((refPt[0][0], refPt[1][0]))
-        y2 = np.max((refPt[0][0], refPt[1][0]))
-        roi = img[x1:x2, y1:y2]
-        if roi.shape[0] > 0 and roi.shape[1] > 0:
-            cv2.imshow('roi', roi)
-            k = cv2.waitKey(0) & 0xFF
-            pnts = np.where(roi > 0)  # contains all possible landmarks at estimated location
-            plt.plot(pnts[1], pnts[0], 'r.')
-            plt.gca().invert_xaxis()
-            plt.gca().invert_yaxis()
-            plt.show()
-        refPt = []
+        elif k == ord('a'):
+            print mouseX, mouseY
 
 if __name__ == '__main__':
     # read an image
--- Segmentation.py
+++ Segmentation.py
@@ -18,14 +18,16 @@ def getMouseCoord(event,x,y,flags,params):
 
 def findClosestPoints(model,pnts):
         pntsC = ASM.center_landmarks(pnts)
+        model = ASM.center_landmarks(model)
         target = np.zeros(model.shape)
+
         for m in xrange(len(model)):
                 dif = pntsC - model[m]
                 px = np.power(dif[:,0],2)
                 py = np.power(dif[:,1],2)
                 d = np.sqrt(px + py)
-                minD = np.min(d)
-                target [m] = pntsC[np.argmin(d).astype(np.int32)]
+
+                target [m] = pnts[np.argmin(d).astype(np.int32)]
 
 
         plt.plot(target[:,0],target[:,1],'r.')
@@ -34,12 +36,50 @@ def findClosestPoints(model,pnts):
 
         return target
 
+def translate(model,target):
+        ctx = np.mean(target[:,0])
+        cty = np.mean(target[:,1])
+        cmx = np.mean(model[:,0])
+        cmy = np.mean(model[:,1])
 
+        plt.plot(model[:,0],model[:,1])
+        plt.show()
+        model[:,0] = model[:,0] - cmx
+        model[:,1] = model[:,1] - cmy
+
+        plt.plot(model[:, 0], model[:, 1])
+
+        model[:,0] = model[:,0] + ctx
+        model[:,1] = model[:,1] + cty
+        plt.plot(model[:, 0], model[:, 1])
+
+        return model
+
+def rotate(model,target):
+        cov = np.dot(target.T, target)
+        n = target.shape[0]
+        cov = np.divide(cov, n - 1)
+        eigW, eigVt = np.linalg.eig(cov)
+
+        cov = np.dot(model.T, model)
+        n = model.shape[0]
+        cov = np.divide(cov, n - 1)
+        eigW, eigVm = np.linalg.eig(cov)
+
+        plt.plot(model[:,0],model[:,1])
+        plt.show()
+
+        rotated = np.dot(model, eigVm)
+        plt.plot(rotated[:,0],rotated[:,1])
+        model = np.dot(rotated, eigVt.T)
+        plt.plot(model[:,0],model[:,1])
+
+        return model
 
 if __name__ == '__main__':
 
 
-
+        # Img.shape = y x c
         img = cv2.imread('_Data/Radiographs/01.tif')
        # img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
@@ -52,7 +92,7 @@ if __name__ == '__main__':
         #model = model.astype(np.int32)
 
         folder = '_Data/landmarks/original/'
-        tooth = 2
+        tooth = 1
         nbImgs = 14
         nbDims = 40
         ASM = ASM(folder,nbImgs,nbDims,tooth)
@@ -93,40 +133,58 @@ if __name__ == '__main__':
                         mouseY = -1
                         refPt = []
                 if len(refPt) >= 2 :
-                        x1 = np.min((refPt[0][1],refPt[1][1]))
-                        x2 = np.max((refPt[0][1],refPt[1][1]))
-                        y1 = np.min((refPt[0][0],refPt[1][0]))
-                        y2 = np.max((refPt[0][0], refPt[1][0]))
+                        x1 = np.min((refPt[0][0],refPt[1][0]))
+                        x2 = np.max((refPt[0][0],refPt[1][0]))
+                        y1 = np.min((refPt[0][1],refPt[1][1]))
+                        y2 = np.max((refPt[0][1], refPt[1][1]))
                         cx = x1 + np.rint((x2-x1)/2)
                         cx = cx.astype(int)
                         cy = y1 + np.rint((y2-y1)/2)
                         cy = cy.astype(int)
-                        roi = img[x1:x2,y1:y2,:]
+                        roi = img[y1:y2,x1:x2,:] # img y,x,c
                         if roi.shape[0]>0 and roi.shape[1]>0 and roi.shape[2]>0:
                                 roi = cv2.cvtColor(roi,cv2.COLOR_BGR2GRAY)
                                 cv2.imshow('roi', roi)
                                 k = cv2.waitKey(20) & 0xFF
-                                pnts = np.where(roi>0) #contains all possible landmarks at estimated location
-                                pnts = np.asarray(pnts).T
-                                plt.plot(pnts[:,1],pnts[:,0],'r.')
+                                pntsYX = np.where(roi>0) #contains all possible landmarks at estimated location
+                                pntsYX = np.asarray(pntsYX).T
+                                pnts = np.zeros(pntsYX.shape,int)
+                                pnts[:,0] = pntsYX[:,1]
+                                pnts[:,1] = pntsYX[:,0]
+
+                                plt.plot(pnts[:,0],pnts[:,1],'r.')
                                 #plt.gca().invert_xaxis()
                                 plt.gca().invert_yaxis()
                                 plt.show()
                         refPt =[]
 
-        target = findClosestPoints(model,pnts)
-        target = target.astype(int)
-        target[:,1] = target[:,1] + cx
-        target[:,0] = target[:,0] + cy
-        for i in xrange(len(target) - 1):
-                cv2.line(img, (target[i, 0], target[i, 1]),
-                         (target[i + 1, 0], target[i + 1, 1]), [0, 0, 255], 2)
+        for i in xrange(10):
+                target = findClosestPoints(model,pnts)
+                target = target.astype(int)
+                
+                target[:,0] = target[:,0] + x1
+                target[:,1] = target[:,1] + y1
+                for i in xrange(len(target) - 1):
+                        cv2.line(img, (target[i, 0], target[i, 1]),
+                                 (target[i + 1, 0], target[i + 1, 1]), [0, 0, 255], 2)
+                        cv2.imshow('img1', img)
+                        k = cv2.waitKey(20) & 0xFF
+                cv2.line(img, (target[-1, 0], target[-1, 1]),
+                         (target[0, 0], target[0, 1]), [0, 0, 255], 2)
                 cv2.imshow('img1', img)
                 k = cv2.waitKey(20) & 0xFF
-        cv2.line(img, (target[-1, 0], target[-1, 1]),
-                 (target[0, 0], target[0, 1]), [0, 0, 255], 2)
-        cv2.imshow('img1', img)
-        k = cv2.waitKey(20) & 0xFF
 
+                model = translate(model,target)
+                model = rotate(model,target)
+                model = model.astype(int)
+                for i in xrange(len(model) - 1):
+                        cv2.line(img, (model[i, 0], model[i, 1]),
+                                 (model[i + 1, 0], model[i + 1, 1]), [0, 255, 0], 2)
+                        cv2.imshow('img1', img)
+                        k = cv2.waitKey(20) & 0xFF
+                cv2.line(img, (model[-1, 0], model[-1, 1]),
+                         (model[0, 0], model[0, 1]), [0, 255,0], 2)
+                cv2.imshow('img1', img)
+                k = cv2.waitKey(20) & 0xFF
 
         print(model)
